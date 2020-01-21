a3f67ba806b700c2b6b99ce1b3f8a4a155f99827
Vignesh <vbhagavath@sapient.com>
2020-01-21 23:30:51 +0800

    merge issues resolved

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Field } from "redux-form";
+import { NavLink } from 'react-router-dom';
 import Input from "../atoms/Input";
 import Button from "../atoms/Button";
 import Logo from "../../assets/images/logo.png";
@@ -7,13 +8,21 @@ class Login extends Component {
   loginHandler = values => {
     const { userLogin } = this.props;
     userLogin(values);
-    this.props.history.push("/add-card");
   };
+
+  componentDidUpdate = prevProps => {
+    const { updateRoute, history, accountNumber } = this.props;
+    if(updateRoute && updateRoute !== prevProps.updateRoute){
+      console.log(accountNumber);
+      history.push('/add-account');
+    }
+  }
 
   render() {
     const { handleSubmit, userVerified } = this.props;
     return (
       <div className="login">
+        { userVerified && <div>You have successfully verified yourself</div>}
         <div className="login-header">
           <div className="login-header__logo">
             <img alt="logo" src={Logo} />
@@ -34,9 +43,9 @@ class Login extends Component {
             name="password"
             placeholder="Password"
           />
-          <a href="" className="signup-link">
+          <NavLink to="/register" className="signup-link">
             Don't have an account yet? Sign up
-          </a>
+          </NavLink>
           <Button className="primary-button" disabled={false}>
             Log in
           </Button>
